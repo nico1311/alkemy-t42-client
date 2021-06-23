3df76ade632bf5c91383ca8382364df1d6ddf0ca
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-06-23 10:43:57 -0300

    Add Material-UI Framework

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import Button from '@material-ui/core/Button';
 
 const App = () => (
   <header className='App-header'>
     <h1>Happy Coding!</h1>
+    <Button variant='contained' color='primary'>
+      Hello World!
+    </Button>
   </header>
 );
 
