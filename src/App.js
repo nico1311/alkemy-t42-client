057ab5cdd7f79c57c6e5097482bcab2b649a5701
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-06-23 10:53:53 -0300

    finish implement Material-UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
 
 const App = () => (
-  <header className='App-header'>
-    <h1>Happy Coding!</h1>
-    <Button variant='contained' color='primary'>
-      Hello World!
-    </Button>
-  </header>
+  <Container maxWidth='sm'>
+    <Box my={4}>
+      <Typography variant='h4' component='h1' gutterBottom>
+        Happy coding!
+      </Typography>
+      <Typography variant='body2' color='textSecondary' align='center'>
+        {'Copyright © '}
+        <Link
+          color='inherit'
+          href='https://bitbucket.org/alkemy-dev/t42-project-client/src/master/'
+        >
+          Team 42 - ONG Proyect
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    </Box>
+  </Container>
 );
 
 export default App;
