f36f129de128a6197fe0fdb0f01d1999a20f7237
Dany-Rolon <dany_rolon@outlook.com>
2021-06-24 10:43:08 -0300

    react-router-dom installed. App wrapped in BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <CssBaseline />
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
