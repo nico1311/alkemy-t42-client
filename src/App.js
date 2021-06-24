3ba1a15933c45d393a824e0e7eeb5877308ea2d4
Dany-Rolon <dany_rolon@outlook.com>
2021-06-24 11:10:20 -0300

    Added Switch and Route imports and some examples, wrapped in Suspense component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
+import React, {Suspense, lazy} from 'react'
+import {Switch, Route} from 'react-router-dom'
 import { UserContextProvider } from 'context/UserContext';
-import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
+import Loader from './utils/Loader/Loader'
+
+const WelcomeDevs = lazy(() => import('./view/welcomeDevs/WelcomeDevs'))
+const AboutUs = lazy(() => import('./view/aboutUs/AboutUs'))
 
 const App = () => (
   <UserContextProvider>
-    <Container maxWidth='sm'>
-      <Box my={4}>
-        <Typography variant='h4' component='h1' gutterBottom>
-          Happy coding!
-        </Typography>
-        <Typography variant='body2' color='textSecondary' align='center'>
-          {'Copyright © '}
-          <Link
-            color='inherit'
-            href='https://bitbucket.org/alkemy-dev/t42-project-client/src/master/'
-          >
-            Team 42 - ONG Proyect
-          </Link>{' '}
-          {new Date().getFullYear()}
-        </Typography>
-      </Box>
-    </Container>
+    <Suspense fallback={Loader}>
+      <Switch>
+        <Route exact path="/" component={WelcomeDevs} />
+        <Route exact path="/nosotros" component={AboutUs}/>
+      </Switch>
+    </Suspense>
   </UserContextProvider>
 );
 
