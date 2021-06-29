79708dfadf997f95359cee30b8e1bf82efc58fbb
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-06-29 12:25:19 -0300

    add rest of prop to extend alert and documentation

--- src/components/utils/alert/AlertGenerator.js
+++ src/components/utils/alert/AlertGenerator.js
@@ -13,6 +13,9 @@ const useStyles = makeStyles(() => ({
  * @param {String} [props.severity="error"] - A string to set "severity" is a type of alert between "error" is default, "warning", "info" and "success".
  * @param {String} [props.alertTitle] - A string to set title of alert.
  * @param {String} [props.contentText] - A string to set content in format text of the alert.
+ * @param {object} [rest] - Rest of props like a variant, color, etc.
+ * @see {@link https://material-ui.com/es/api/alert/}
+ * @see {@link https://material-ui.com/es/components/alert/}
  * @example
  * import AlertGenerator from "components/utils/alert/AlertGenerator.js"
  * <AlertGenerator />
@@ -20,11 +23,18 @@ const useStyles = makeStyles(() => ({
  * <AlertGenerator contentText="This is a alert warning" severity="warning" />
  * <AlertGenerator contentText="This is a alert info" severity="info" />
  * <AlertGenerator contentText="This is a alert sucess" severity="success" />
+ * <AlertGenerator contentText="This is a alert sucess" severity="success" variant="filled" />
+ * <AlertGenerator contentText="This is a alert sucess" severity="success" variant="outline" />
  */
-const AlertGenerator = ({ severity = 'error', alertTitle, contentText }) => {
+const AlertGenerator = ({
+  severity = 'error',
+  alertTitle,
+  contentText,
+  ...rest
+}) => {
   const { alertStyle } = useStyles();
   return (
-    <Alert className={alertStyle} severity={severity}>
+    <Alert className={alertStyle} severity={severity} {...rest}>
       <AlertTitle>{alertTitle}</AlertTitle>
       {contentText}
     </Alert>
