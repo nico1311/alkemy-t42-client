7de09923d5a55cf5cd3bc50226fc2e50d477212e
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-07-01 18:37:09 -0300

    make a component to render route signUp

---
+++ src/view/signup/SignUp.js
@@ -0,0 +1,14 @@
+/** @module Routes/SignUp */
+import FormRegister from 'components/form/register/FormRegister';
+/**
+ * SignUpPage is a initial/index page for route /signup.
+ * @fuction SignUpPage
+ * @example
+ * <SignUpPage />
+ * @example
+ * // With a Router.
+ * <Route exact path="/" component={SignUpPage} />
+ */
+const SignUpPage = () => <FormRegister />;
+
+export default SignUpPage;
