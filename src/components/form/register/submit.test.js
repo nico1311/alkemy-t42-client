af0a76d7342deb912ab9d14e7d02369621ba2cf4
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-07-02 13:00:24 -0300

    add test for error alert

--- src/components/form/register/submit.test.js
+++ src/components/form/register/submit.test.js
@@ -47,4 +47,39 @@ describe('/component/form/register/submit.js - Result function is fine', () => {
       expect(handleSubmit.mock.calls.length).toBe(1);
     });
   });
+  test('Click on button submit call function default and fail event', async () => {
+    render(
+      <MemoryRouter>
+        <FormRegister />
+      </MemoryRouter>,
+    );
+    const inputFirstName = screen.getByRole('textbox', {
+      name: 'First Name',
+    });
+    fireEvent.change(inputFirstName, {
+      target: { value: 'Brendan' },
+    });
+    const inputLastName = screen.getByRole('textbox', {
+      name: 'Last Name',
+    });
+    fireEvent.change(inputLastName, {
+      target: { value: 'Eich' },
+    });
+    const inputEmail = screen.getByRole('textbox', {
+      name: 'Email Address',
+    });
+    fireEvent.change(inputEmail, {
+      target: { value: 'BrendanEich@gmail.com' },
+    });
+    const inputPassword = screen.getByLabelText(/Password/i, {
+      selector: 'input',
+    });
+    fireEvent.change(inputPassword, {
+      target: { value: '123456' },
+    });
+    fireEvent.click(screen.getByRole('button'));
+    await waitFor(() => {
+      screen.getByText('Sorry we have a error. Contact with support, please.');
+    });
+  });
 });
