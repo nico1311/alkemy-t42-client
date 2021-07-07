58edb95ce7840108312ab31855d027f17be0299c
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-07-06 21:10:59 -0300

    add action constant for user reducer

--- src/redux/constants/constants.js
+++ src/redux/constants/constants.js
@@ -2,3 +2,9 @@ export const actionType = {
   GET_WELCOME_TEXT: 'GET_WELCOME_TEXT',
   GET_NEWS: 'GET_NEWS',
 };
+
+export const actionUser = {
+  USER_GET: 'USER_GET',
+  USER_LOGIN: 'USER_LOGIN',
+  USER_LOGOUT: 'USER_LOGOUT',
+};
