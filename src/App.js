de4e1a3e677766ab2ac6ef679aa113504390d55e
lggerman <lopez.ggerman@gmail.com>
2021-07-08 20:17:38 -0300

    added animation component to app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React, { Suspense, lazy } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, useLocation } from 'react-router-dom';
 import Loader from './components/utils/Loader/Loader';
 import { ThemeProvider } from '@material-ui/core';
 import theme from './theme';
+import { AnimatePresence, motion } from 'framer-motion';
 
-const MainView = lazy(() => import('view/mainView/MainView'))
-const BackOfficeView = lazy(() => import('view/backOfficeView/BackOfficeView'))
+const MainView = lazy(() => import('view/mainView/MainView'));
+const BackOfficeView = lazy(() => import('view/backOfficeView/BackOfficeView'));
 
-const App = () => (
-  <Suspense fallback={<Loader />}>
-    <ThemeProvider theme={theme}>
-      <Switch>
-        <Route path='/backoffice' component={BackOfficeView} /> {/** This should be a Private Route */}
-        <Route path='/' component={MainView} />
-      </Switch>
-    </ThemeProvider>
-  </Suspense>
-);
+const App = () => {
+  const location = useLocation();
+  return (
+    <Suspense fallback={<Loader />}>
+      <ThemeProvider theme={theme}>
+        <AnimatePresence exitBeforeEnter>
+          <Switch location={location} key={location.pathname}>
+            <motion.div
+              initial={{ opacity: 0, width: '100%' }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <Route path='/backoffice' component={BackOfficeView} />{' '}
+              {/** This should be a Private Route */}
+              <Route path='/' component={MainView} />
+            </motion.div>
+          </Switch>
+        </AnimatePresence>
+      </ThemeProvider>
+    </Suspense>
+  );
+};
 
 export default App;
