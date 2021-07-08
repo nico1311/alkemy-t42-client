98112b95e9691ae35682831aa439660b2c10c8f8
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-07-08 13:29:51 -0300

    set a default submit to test

--- src/components/form/news/submit.js
+++ src/components/form/news/submit.js
@@ -1,5 +1,10 @@
 /** @module Form/News */
 
-const submit = async (values, setSubmit) => {};
+const submit = async (values, setSubmit) => {
+  setTimeout(() => {
+    console.log(values);
+    setSubmit(false);
+  }, 2000);
+};
 
 export default submit;
