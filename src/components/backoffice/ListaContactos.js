59f4633d053db7007c674814adcdd0f1993a640e
Federico Bravin <fedeinfo31@gmail.com>
2021-07-09 17:36:42 -0300

    Add contact list.

--- src/components/backoffice/ListaContactos.js
+++ src/components/backoffice/ListaContactos.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -7,12 +7,18 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import { makeGET } from 'services/httpRequest';
 import { ENDPOINT_CONTACTS} from 'services/settings';
 import useFetch from 'hooks/useFetch';
 import { getToken } from 'services/tokenHandler'
 
+const useStyles = makeStyles({
+  table: {
+    minWidth: 400,
+  },
+});
+
 export default function ListOfContacts() {
+
   const { response, error, loading } = useFetch(ENDPOINT_CONTACTS, 
     {
       method: "GET",
@@ -22,6 +28,8 @@ export default function ListOfContacts() {
       },
     }
   )
+
+  const classes = useStyles();
   
   console.log(response)
 
@@ -31,125 +39,41 @@ export default function ListOfContacts() {
     )
   }
 
-  /*return(
-    <>
-      {response.contacts.map(res =>
-        (<h1> {res.name} </h1>)  
-      )}
-    </> 
-  )*/
-}
-
-/*const getContacts = () => {
-  try {
-    return makeRequest(`${ENDPOINT_CONTACT}`, "GET");
-  } catch (error) {
-    console.log(error);
+  if(response?.contacts){
+    return (
+      <>
+        <h1 align='center'> Contactos</h1>
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">Nombre</TableCell>
+                <TableCell align="center">Email</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {response.contacts.map((res) => (
+                <TableRow key={res.id}>
+                  <TableCell align="center">{res.name}</TableCell>
+                  <TableCell align="center">{res.email}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </>
+    );
   };
-};
 
-console.log(getContacts);*/
-
-/*const ListOfContact = () => {
-  
-  return (
-          <h1>Hola</h1>  
-  );
-};
-
-export default ListOfContact;*/
-
-
-
-/*const StyledTableCell = withStyles((theme) => ({
-  head: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  body: {
-    fontSize: 14,
-  },
-}))(TableCell);
-
-const StyledTableRow = withStyles((theme) => ({
-  root: {
-    '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.action.hover,
-    },
-  },
-}))(TableRow);
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+  return null;
 }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-const useStyles = makeStyles({
-  table: {
-    minWidth: 700,
-  },
-});
-
-export default function CustomizedTables() {
-  const classes = useStyles();
-
-  return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
-            <StyledTableCell align="right">Calories</StyledTableCell>
-            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow key={row.name}>
-              <StyledTableCell component="th" scope="row">
-                {row.name}
-              </StyledTableCell>
-              <StyledTableCell align="right">{row.calories}</StyledTableCell>
-              <StyledTableCell align="right">{row.fat}</StyledTableCell>
-              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-              <StyledTableCell align="right">{row.protein}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
-}*/
 
 
 
-/*export default function ListOfContacts() {
-    const [ contacts, setContacts ] = useState([]);
 
-    useEffect(() => {
-        axios
-            .get("http://localhost:4000/api/controllers/ContactsController'")
-            .then((res) => {
-                const data = res.data;
-                setContacts(data);
-            })
-            .catch((err) => console.log(err));
-    }, []);
 
-    useEffect(() => {
-        console.log(contacts)
-    }, [contacts])
-    
-    return console.log(contacts)
-}
 
-console.log()*/
+
+
+
