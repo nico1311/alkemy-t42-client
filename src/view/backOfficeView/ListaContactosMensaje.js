462b1888fe7042c9749ada25cbb5d87f28ab9fa2
Federico Bravin <fedeinfo31@gmail.com>
2021-07-10 16:40:58 -0300

    Adding new ContactosMensaje.js.

---
+++ src/view/backOfficeView/ListaContactosMensaje.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ListaContactosMensaje from '../../components/backoffice/ListaContactosMensaje';
+
+const ContactosMensaje = () => {
+  return <ListaContactosMensaje />;
+};
+
+export default ContactosMensaje;
