cfd8b3890cb0da14d59d638f29186906d1d9939b
Celeste <celestecabrera.2711@gmail.com>
2021-07-10 15:49:12 -0300

    Solving conflicts in MainView component

--- src/view/mainView/MainView.js
+++ src/view/mainView/MainView.js
@@ -16,28 +16,34 @@ const SignUp = lazy(() => import('view/signup/SignUp'));
 const SignIn = lazy(() => import('view/signin/SignIn'));
 const Contact = lazy(() => import('view/contact/Contact'));
 const News = lazy(() => import('view/news/News'))
+const SingleNew = lazy(() => import('view/singleNew/SingleNew'))
+const MyProfile = lazy(() => import('view/myProfile/MyProfile'));
+const EditUserPage = lazy(() => import('view/editUser/editUserPage'));
+
 
 const MainView = () => {
-  return (
-    <div>
-      <AnimatePresence exitBeforeEnter>
+    return (
+        <AnimatePresence exitBeforeEnter>
         <motion.div initial={initial} animate={animate} exit={exit}>
-          <Header />
-          <Grid container>
-            <Switch>
-              <Route path='/nosotros' component={AboutUs} />
-              <Route path='/contacto' component={Contact} />
-              <Route path='/registrar' component={SignUp} />
-              <Route path='/ingresar' component={SignIn} />
-              <Route path='/novedades' component={News} />
-              <Route path='/' component={HomePage} />
-            </Switch>
-          </Grid>
-          <Footer />
-        </motion.div>
+            <Header />
+            <Grid container>
+                <Switch>
+                    <Route path='/nosotros' component={AboutUs} />
+                    <Route path='/contacto' component={Contact} />
+                    <Route path='/registrar' component={SignUp} />
+                    <Route path='/ingresar' component={SignIn} />
+                    <Route path='/perfil/editar' component={EditUserPage}></Route>
+                    <Route path='/novedades/:id' render={({ match }) => (<SingleNew id={match.params.id}/>)} />
+                    <Route path='/novedades' component={News} />
+                    <Route path='/perfil' component={MyProfile} ></Route>
+                    <Route path='/' component={HomePage} />
+                </Switch>
+            </Grid>
+            <Footer />
+            </motion.div>
       </AnimatePresence>
-    </div>
-  );
-};
+    )
+}
+
 
 export default MainView;
