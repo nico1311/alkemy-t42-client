d119a1099a96ed9d0f768ae7bac373bc8d1679a6
Dany-Rolon <dany_rolon@outlook.com>
2021-07-14 10:13:26 -0300

    Chage path /backoffice to be private

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Suspense, lazy, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
+import PrivateRoute from 'components/utils/PrivateRoute/PrivateRoute'
 import Loader from './components/utils/Loader/Loader';
 import { ThemeProvider } from '@material-ui/core';
 import theme from './theme';
 import { makeGET } from 'services/httpRequest';
 import { ENDPOINT_GETLOGGED } from 'services/settings';
 import { getLoggedUser } from 'redux/user/actions/user';
+import { getToken } from 'services/tokenHandler'
 
 const MainView = lazy(() => import('view/mainView/MainView'));
 const BackOfficeView = lazy(() => import('view/backOfficeView/BackOfficeView'));
@@ -15,13 +17,15 @@ const App = () => {
   const dispatch = useDispatch()
   // Set user on APP if have a token in LocalStorage.
   useEffect(() => {
-    makeGET(ENDPOINT_GETLOGGED).then((res) => dispatch(getLoggedUser(res)));
+    if(getToken()){
+      makeGET(ENDPOINT_GETLOGGED).then((res) => dispatch(getLoggedUser(res)));
+    }
   }, [dispatch]);
   return (
       <Suspense fallback={<Loader />}>
         <ThemeProvider theme={theme}>
           <Switch>
-            <Route path='/backoffice' component={BackOfficeView} />
+            <PrivateRoute path='/backoffice' component={BackOfficeView} redirectTo={'/'} shouldBeAdmin={false} />
             <Route path='/' component={MainView} />
           </Switch>
         </ThemeProvider>
