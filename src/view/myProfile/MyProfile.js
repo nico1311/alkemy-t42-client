577638411fcf4a0a93f4eb935da7cb99e54dbfed
unknown <cabreraceleste672@gmail.com>
2021-07-22 12:12:46 -0300

    Changed Profile view to make it show the logged user data

--- src/view/myProfile/MyProfile.js
+++ src/view/myProfile/MyProfile.js
@@ -1,19 +1,30 @@
-import { React } from 'react';
+import { React, useState, useEffect } from 'react';
 import { useHistory, useRouteMatch } from 'react-router-dom';
 import { Grid } from '@material-ui/core';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import MailIcon from '@material-ui/icons/Mail';
 import FaceIcon from '@material-ui/icons/Face';
-import CardProfile from 'components/profile/CardProfile';
+import CardProfile from 'components/Profile/CardProfile';
 import Button from '@material-ui/core/Button';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import useStyles from './style';
+import {makeGET} from 'services/httpRequest';
+import {ENDPOINT_GETLOGGED} from 'services/settings';
 
 function MyProfile() {
   const classes = useStyles();
   const history = useHistory();
   const { url } = useRouteMatch();
+  const [usuario, setUsuario] = useState();
+
+  useEffect(() => {
+    const fetchUsuario = async () => {
+      const data = await makeGET(ENDPOINT_GETLOGGED);
+      setUsuario(data);
+    }
+    fetchUsuario();
+  }, []);
 
   const onClickHandler = (route) => {
     history.push(`${url}${route}`);
@@ -25,21 +36,21 @@ function MyProfile() {
         <CardProfile
           icono={<FaceIcon className={classes.iconos} />}
           titulo='Nombre'
-          texto='Leandro'
+          texto={usuario ? usuario.firstName : 'Cargando...'}
         />
       </Grid>
       <Grid item xs={10} md={8} xl={4}>
         <CardProfile
           icono={<AccountCircleIcon className={classes.iconos} />}
           titulo='Apellido'
-          texto='Garassino'
+          texto={usuario ? usuario.lastName : 'Cargando...'}
         />
       </Grid>
       <Grid item xs={10} md={8} xl={4}>
         <CardProfile
           icono={<MailIcon className={classes.iconos} />}
           titulo='Email'
-          texto='leandro.garassino@gmail.com'
+          texto={usuario ? usuario.email : 'Cargando...'}
         />
       </Grid>
       <Grid container className={classes.root}>
