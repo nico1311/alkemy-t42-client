3505df52434b6287724da6540a7de08a03262ac2
Federico Bravin <fedeinfo31@gmail.com>
2021-07-24 16:47:20 -0300

    Functional edit section. Added new files to work with. Add new get endpoint in category to work well.

--- src/view/backOfficeView/BackOfficeView.js
+++ src/view/backOfficeView/BackOfficeView.js
@@ -30,7 +30,8 @@ const EditUserPage = lazy(() => import('view/editUser/editUserPage'));
 const ListadoTestimonios = lazy(() => import('components/listadoTestimonios/ListadoTestimonios'));
 const CreateTestimonial = lazy(() => import('./FormTestimonial'))
 const CategoriesView = lazy(() => import('../backoffice/Categories/Categories'));
-const FormCategory = lazy(() => import('components/form/category/FormCategory'))
+const FormCategory = lazy(() => import('components/form/category/FormCategory'));
+const FormNews = lazy(() => import('components/form/news/FormNews'));
 const UsersView = lazy(() => import('../backoffice/Users/Users'));
 
 
@@ -116,8 +117,16 @@ function BackOfficeView() {
                 component={FormCategory} 
               />
               <Route 
+                path={`${path}/categories/edit`} 
+                component={FormCategory} 
+              />
+              <Route 
                 path={`${path}/testimonials/create`} 
                 component={CreateTestimonial} 
+              />
+              <Route 
+                path={`${path}/news/create`} 
+                component={FormNews}
               />
               <Route 
                 path={`${path}/users`} 
--- src/view/backoffice/Categories/Categories.js
+++ src/view/backoffice/Categories/Categories.js
@@ -11,13 +11,14 @@ import TableBody from '@material-ui/core/TableBody';
 import TableHead from '@material-ui/core/TableHead';
 import Snackbar from '@material-ui/core/Snackbar';
 import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button'
 
 import CloseIcon from '@material-ui/icons/Close';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 
+import EditCategoryForm from 'components/form/category/editFormCategory';
 import AlertDelete from 'components/utils/alertDelete/AlertDelete';
 import useStyles from './style';
 
@@ -25,36 +26,45 @@ import { makeGET, makeDELETE } from 'services/httpRequest';
 import { ENDPOINT_CATEGORY } from 'services/settings';
 
 const Categories = () => {
-  const { url } = useRouteMatch();
+  const {url} = useRouteMatch();
   const history = useHistory();
   const [categories, setCategories] = useState([]);
+  const [edit, setEdit] = useState(false);
+  const [categoryToEdit, setCategoryToEdit] = useState('');
   const [pendingCategory, setPendingCategory] = useState(null);
   const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
   const [toastOpen, setToastOpen] = useState(false);
 
   const classes = useStyles();
   useEffect(() => {
-    const fetchCategories = async () => {
-      const data = await makeGET(ENDPOINT_CATEGORY);
-      setCategories(data.categories);
-    };
-    fetchCategories();
+    getCategories();
+    return () => {};
   }, []);
+
+  const getCategories = async () => {
+    const categories = await makeGET(ENDPOINT_CATEGORY);
+    setCategories(categories.categories);
+  };
+
+  const editCategory = async (id) => {
+    const response = await makeGET(`${ENDPOINT_CATEGORY}/${id}`);
+    console.log(response);
+    setCategoryToEdit(response.Category);
+    setEdit(true);
+  };
 
   const handleDeleteAction = (categoryId) => {
     setPendingCategory(categories.find((cat) => cat.id === categoryId));
     setDeleteDialogOpen(true);
-  };
+  }
 
   const handleDeleteCancel = () => {
     setDeleteDialogOpen(false);
     setPendingCategory(null);
-  };
+  }
 
   const handleDeleteConfirm = async () => {
-    const result = await makeDELETE(
-      `${ENDPOINT_CATEGORY}/${pendingCategory.id}`,
-    );
+    const result = await makeDELETE(`${ENDPOINT_CATEGORY}/${pendingCategory.id}`);
     if (result) {
       setCategories(categories.filter((cat) => cat.id !== pendingCategory.id));
       setToastOpen(true);
@@ -64,89 +74,117 @@ const Categories = () => {
     setPendingCategory(null);
   };
 
-  return (
-    <>
-      <Container>
-        <div style={{ width: '100%' }}>
-          <Box display='flex'>
-            <Box width='100%'>
-              {' '}
-              <Typography variant='h4'> Categorias </Typography>{' '}
-            </Box>
-            <Box>
-              {' '}
-              <Button
-                onClick={() => history.push(`${url}/create`)}
-                variant='contained'
-                color='primary'
-              >
-                Crear
-              </Button>{' '}
-            </Box>
-          </Box>
-        </div>
-        <TableContainer>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Nombre</TableCell>
-                <TableCell className={classes.right}>Acciones</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {categories.map((category, i) => {
-                return (
-                  <TableRow key={i}>
-                    <TableCell>{category.name}</TableCell>
-                    <TableCell className={classes.right}>
-                      <Button
-                        variant='contained'
-                        color='primary'
-                        className={classes.button}
-                        startIcon={<EditIcon className={classes.icon} />}
-                        onClick={() => {
-                        {/*editActivity(activity.id);*/}
-                        }}
-                      >
-                        Editar
-                      </Button>
-                      <Button
-                        variant='contained'
-                        color='secondary'
-                        startIcon={<DeleteIcon className={classes.icon} />}
-                        className={classes.button}
-                        onClick={() => {
-                          handleDeleteAction(category.id);
-                        }}
-                      >
-                        Eliminar
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Container>
-      {pendingCategory &&
-        <AlertDelete
-          message={`¿Eliminar la categoría "${pendingCategory.name}"?`}
-          open={deleteDialogOpen}
-          cancelar={handleDeleteCancel}
-          confirmar={handleDeleteConfirm}
-          onClose={handleDeleteCancel}
-          snackbarOpen={toastOpen}
-          snackbarMessage='Categoría eliminada'
-          onSnackbarClose={() => setToastOpen(false)}
-        />
-      }
-    
+  if (edit) {
+    return (
+      <EditCategoryForm
+        getCategories={getCategories}
+        categoryToEdit={categoryToEdit}
+        setEdit={setEdit}
+      ></EditCategoryForm>
     );
-    } else {
-      return <h1>No hay categorias para mostrar.</h1>;
-    }
-    </>
-  };
-};
+  }
+
+  if (categories){
+    return (
+      <>
+        <Container>
+          <div style={{ width: '100%' }}>
+            <Box display='flex'>
+              <Box width='100%'>
+                {' '}
+                <Typography variant='h4'> Categorias </Typography>{' '}
+              </Box>
+              <Box>
+                {' '}
+                <Button
+                  onClick={() => history.push(`${url}/create`)}
+                  variant='contained'
+                  color='primary'
+                >
+                  Crear
+                </Button>{' '}
+              </Box>
+            </Box>
+          </div>
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Nombre</TableCell>
+                  <TableCell className={classes.right}>Acciones</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {categories.map((category, i) => {
+                  return (
+                    <TableRow key={i}>
+                      <TableCell>{category.name}</TableCell>
+                      <TableCell className={classes.right}>
+                        <Button
+                          variant='contained'
+                          color='primary'
+                          className={classes.button}
+                          startIcon={<EditIcon className={classes.icon} />}
+                          onClick={() => {
+                            editCategory(category.id);
+                          }}
+                        >
+                          Editar
+                        </Button>
+                        <Button
+                          variant='contained'
+                          color='secondary'
+                          startIcon={<DeleteIcon className={classes.icon} />}
+                          className={classes.button}
+                          onClick={() => {
+                            handleDeleteAction(category.id);
+                          }}
+                        >
+                          Eliminar
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Container>
+        {pendingCategory &&
+          <AlertDelete
+            message={`¿Eliminar la categoría "${pendingCategory.name}"?`}
+            open={deleteDialogOpen}
+            cancelar={handleDeleteCancel}
+            confirmar={handleDeleteConfirm}
+            onClose={handleDeleteCancel}
+            snackbarOpen={toastOpen}
+            snackbarMessage='Categoría eliminada'
+            onSnackbarClose={() => setToastOpen(false)}
+          />
+        }
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          open={toastOpen}
+          autoHideDuration={2000}
+          onClose={() => setToastOpen(false)}
+          message='Categoría eliminada'
+          action={
+            <IconButton
+              size='small'
+              aria-label='close'
+              color='inherit'
+              onClick={() => setToastOpen(false)}
+            >
+              <CloseIcon fontSize='small' />
+            </IconButton>
+          }
+        />
+      </>
+    );
+  }
+}
+
 export default Categories;
